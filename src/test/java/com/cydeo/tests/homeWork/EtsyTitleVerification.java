9bcfa62f9966d42d759b3ec341e4ce65c60dd846
kalbi <kalbinur.duzkalem@outlook.com>
2022-12-23 12:50:53 +0300

    Selenium Homework

--- src/test/java/com/cydeo/tests/homeWork/EtsyTitleVerification.java
+++ src/test/java/com/cydeo/tests/homeWork/EtsyTitleVerification.java
@@ -1,16 +1,42 @@
 package com.cydeo.tests.homeWork;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class EtsyTitleVerification {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
-      // TC #1: Etsy Title Verification
-      // 1. Open Chrome browser
-      // 2. Go to https://www.etsy.com
-      // 3. Search for “wooden spoon”
-      // 4. Verify title:
-      // Expected: “Wooden spoon | Etsy "
+        // TC #1: Etsy Title Verification
+        WebDriverManager.chromedriver().setup();
+
+        // 1. Open Chrome browser
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        // 2. Go to https://www.etsy.com
+        driver.get("https://www.etsy.com");
+
+        // 3. Search for “wooden spoon”
+        WebElement searchBox = driver.findElement(By.name("search_query"));
+        searchBox.sendKeys("wooden spoon"+ Keys.ENTER);
+
+
+        // 4. Verify title:
+        // Expected: “Wooden spoon | Etsy
+        String expectedTitle = "Wooden spoon | Etsy";
+        String actuallyTitle= driver.getTitle();
+
+        if (expectedTitle.equals(actuallyTitle)){
+            System.out.println("Title verification PASSED!");
+        } else{
+            System.out.println("Title verification FAILED!");
+        }
 
     }
 }

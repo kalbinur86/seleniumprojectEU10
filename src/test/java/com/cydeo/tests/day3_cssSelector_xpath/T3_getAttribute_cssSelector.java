ad80b70e7a92403336b7f52d975442398df848bf
kalbi <kalbinur.duzkalem@outlook.com>
2023-01-03 16:29:55 +0300

    Selenium

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T3_getAttribute_cssSelector.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T3_getAttribute_cssSelector.java
@@ -8,37 +8,40 @@ import org.openqa.selenium.WebElement;
 public class T3_getAttribute_cssSelector {
 
     public static void main(String[] args) {
-
-       // TC #3: NextBaseCRM, locators, getText(), getAttribute() practice
-       // 1- Open a Chrome browser
-       WebDriver driver = WebDriverFactory.getDriver("chrome");
+        //TC #3: NextBaseCRM, locators, getText(), getAttribute() practice
+        //1- Open a Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
 
-       // 2- Go to: https://login1.nextbasecrm.com/
+        //2- Go to: https://login1.nextbasecrm.com/
         driver.get("https://login1.nextbasecrm.com/");
 
-        // 3- Verify “Log in” button text is as expected:
-       // Expected: Log In
-      //  WebElement signInButton =  driver.findElement(By.className("login-btn"));
+        //3- Verify “Log in” button text is as expected:
+        //Expected: Log In
+        //WebElement signInButton = driver.findElement(By.className("login-btn"));
 
+        //LOCATING THE SAME WEB ELEMENT USING DIFFERENT ATTRIBUTE VALUES.
+        //	                   tagName[attribute='value']
+        //	                   input[class='login-btn']
+        // LOCATED USING CLASS ATTRIBUTE
+        //WebElement signInButton = driver.findElement(By.cssSelector("input[class='login-btn']"));
+        // LOCATED USING TYPE ATTRIBUTE
+        //WebElement signInButton = driver.findElement(By.cssSelector("input[type='submit']"));
+        // LOCATED USING VALUE ATTRIBUTE
+        WebElement signInButton = driver.findElement(By.cssSelector("input[value='Log In']"));
 
-    // locating the same web element using different attribute value.
-        //               tagName[attribute='value'] // as below
-        //               input[class='login-btn']
-        //or             input[type='submit']
-        //or             input[value='log In']
+        String expectedButtonText = "Log In";
 
-    //  WebElement signInButton = driver.findElement(By.cssSelector("input[class='login-btn']"));
-    //  WebElement signInButton = driver.findElement(By.cssSelector("input[type='submit']"));
-        WebElement signInButton = driver.findElement(By.cssSelector("input[value='log In']"));
+        //Getting the value of the attribute "value"
+        String actualButtonText = signInButton.getAttribute("value");
 
+        System.out.println("actualButtonText = " + actualButtonText);
 
-       // PS: Inspect and decide which locator you should be using to locate web
-       // elements.
-       // PS2: Pay attention to where to get the text of this button from
-
-
-
-
+        if (actualButtonText.equals(expectedButtonText)){
+            System.out.println("Log In button text verification passed!");
+        }else{
+            System.out.println("Log In button text verification failed!");
+        }
     }
 }
+
--- src/test/java/com/cydeo/utilities/WebDriverFactory.java
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -1,37 +1,38 @@
 package com.cydeo.utilities;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+// TASK: NEW METHOD CREATION
+// Method name : getDriver
+// Static method
+// Accepts String arg: browserType
+//   - This arg will determine what type of browser is opened
+//   - if "chrome" passed --> it will open Chrome browser
+//   - if "firefox" passed --> it will open firefox browser
+// RETURN TYPE: "WebDriver"
+
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.*;
 
 public class WebDriverFactory {
 
+    public static WebDriver getDriver(String browserType){
 
-    // TASK : NEW METHOD CREATION
-    // METHOD name : getDriver
-    // Static method
-    // Accepts String arg : browserType
-    // -This arg will determine what type of browser is opened
-    // -if "Chrome" passed --> it will open chrome browser
-    // -if "firefox" passed --> it will open firefox browser
-    //RETURN TYPE : "WebDriver"
-
-    public static WebDriver getDriver(String browserType) {
-
-        if (browserType.equalsIgnoreCase("chrome")) {
+        if (browserType.equalsIgnoreCase("chrome")){
 
             WebDriverManager.chromedriver().setup();
             return new ChromeDriver();
 
-        } else if (browserType.equalsIgnoreCase("firefox")) {
+        }else if (browserType.equalsIgnoreCase("firefox")){
             WebDriverManager.firefoxdriver().setup();
             return new FirefoxDriver();
-        } else {
+        }else{
             System.out.println("Given browser type does not exist/or is not currently supported");
+            System.out.println("Driver = null");
             return null;
         }
 
 
     }
+
 }

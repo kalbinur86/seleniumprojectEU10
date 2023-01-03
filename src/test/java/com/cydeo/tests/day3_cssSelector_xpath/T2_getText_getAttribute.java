ca3e84dfcb8e58fa8b8b547d9af4a8896bbc0690
kalbi <kalbinur.duzkalem@outlook.com>
2023-01-03 15:20:39 +0300

    Selenium

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T2_getText_getAttribute.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T2_getText_getAttribute.java
@@ -24,14 +24,39 @@ public class T2_getText_getAttribute {
        String expectedRememberMeLabel = " Remember me on this computer";
        String actualRememberMeLabel = rememberMeLabel.getText();
 
+       if (actualRememberMeLabel.equals(expectedRememberMeLabel)){
+           System.out.println("Label verificatin PASSED!");
+       }else {
+           System.out.println("Label verification FAILED!");
+       }
+
        // 4- Verify “forgot password” link text is as expected:
        // Expected: Forgot your password?
+       WebElement forgetPasswordLinkText = drive.findElement(By.className("login-link-forgot-pass"));
+
+       String expectedForgetPasswordLinkText = "Forget your password?";
+       String actuallyForgetPasswordLinkText = forgetPasswordLinkText.getText();
+
+       if (actuallyForgetPasswordLinkText.equals(expectedForgetPasswordLinkText)){
+           System.out.println("Forget password link verification PASSED!");
+       }else {
+           System.out.println("Forget password link verification FAILED!");
+       }
+
+
        // 5- Verify “forgot password” href attribute’s value contains expected:
        // Expected: forgot_password=yes
+       String expectedInHref = " forgot_password=yes";
+       String actualHrefAttributeValue = forgetPasswordLinkText.getAttribute("href");
+
+       System.out.println("actualHrefAttributeValue"+actualHrefAttributeValue);
 
 
-
-
+      if (actualHrefAttributeValue.contains(expectedInHref)){
+          System.out.println("HREF attribute value verification PASSED!");
+      }else {
+          System.out.println("HREF attribute value verification FAILED!");
+      }
 
 
 

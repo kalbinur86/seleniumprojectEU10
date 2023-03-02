dfd7cfec348b98ea55e0cd3edab7e93652782d38
kalbi <kalbinur.duzkalem@outlook.com>
2023-03-02 22:32:09 +0300

    Selenium

--- src/test/java/com/cydeo/tests/day6_alert_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alert_iframes_windows/DropdownPractices.java
@@ -5,46 +5,103 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
 
+    public WebDriver driver;
+
+
+
+
+    @BeforeMethod
+    public void setupMethod(){
+        //  1. Open Chrome browser
+        //  2. Go to http://practice.cybertekschool.com/dropdown
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/dropdown");
+    }
+
     @Test
     public void dropdown_task5() throws InterruptedException {
 
-      //  1. Open Chrome browser
-      //  2. Go to http://practice.cybertekschool.com/dropdown
-        WebDriver drive = WebDriverFactory.getDriver("chrome");
-        drive.manage().window().maximize();
-        drive.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        drive.get("http://practice.cybertekschool.com/dropdown");
-
         // we locate the dropdown and it is ready to use
-        Select stateDropdown = new Select(drive.findElement(By.xpath("//select[@id='state']")));
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
 
-      //  3. Select Illinois
+        //  3. Select Illinois
+        //  Use all Select options. (visible text, value, index)
         Thread.sleep(1000);
         stateDropdown.selectByVisibleText("Illinois");
 
-      //  4. Select Virginia
+       //  4. Select Virginia
         stateDropdown.selectByValue("VA");
 
-      //  5. Select California
+       //  5. Select California
         stateDropdown.selectByIndex(5);
 
-      //  6. Verify final selected option is California.
+       //  6. Verify final selected option is California.
 
         String expectedOptionText = "California";
         String actualOptionText = stateDropdown.getFirstSelectedOption().getText();
 
         Assert.assertEquals(actualOptionText,expectedOptionText);
+    }
+
+    @Test
+    public void dropdown_task6(){
+
+            // Select "December 1, 1924" and verify it is selected.
+        Select yearDropdown = new Select(driver.findElement(By.xpath("//select[@id='year']")));
+        Select monthDropdown = new Select(driver.findElement(By.xpath("//select[@id='month']")));
+        Select dayDropdown = new Select(driver.findElement(By.xpath("//select[@id='day']")));
+
+            // Select year using : visible test
+        yearDropdown.selectByVisibleText("1924");
+
+            // Select month using : value attribute
+        monthDropdown.selectByValue("11");
+
+            // Select day using : index number
+        dayDropdown.selectByIndex(0);
 
 
+          // creating expected values
+        String expectedYear = "1924";
+        String expectedMonth = "December";
+        String expectedDay = "1";
 
-      //  Use all Select options. (visible text, value, index)
+        //getting actual value from browser
+        String actualYear = yearDropdown.getFirstSelectedOption().getText();
+        String actualMonth = monthDropdown.getFirstSelectedOption().getText();
+        String actualDay = dayDropdown.getFirstSelectedOption().getText();
 
+        //create assertions
+        Assert.assertTrue(actualYear.equals(expectedYear));
+        Assert.assertEquals(actualMonth, expectedMonth);
+        Assert.assertEquals(actualDay, expectedDay);
 
     }
+
+    @AfterMethod
+    public void teardownMethod(){
+       driver.close();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }

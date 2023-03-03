1e40a744e68b789ea0509655b7b8ff0dd807a3b7
kalbi <kalbinur.duzkalem@outlook.com>
2023-03-03 09:28:36 +0300

    Selenium

---
+++ src/test/java/com/cydeo/tests/day7_webtable_utilities_javafaker/T1_WindowHandling.java
@@ -0,0 +1,45 @@
+package com.cydeo.tests.day7_webtable_utilities_javafaker;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_WindowHandling {
+
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void window_handling_test() {
+        //2. Go to: https://www.amazon.com
+        driver.get("https://www.amazon.com");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

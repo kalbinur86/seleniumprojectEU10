dbe2cc3996c9b9a0c3e5131c52659a53d36a03ad
kalbi <kalbinur.duzkalem@outlook.com>
2023-03-03 12:50:10 +0300

    Selenium

--- src/test/java/com/cydeo/utilities/BrowserUtilities.java
+++ src/test/java/com/cydeo/utilities/BrowserUtilities.java
@@ -2,7 +2,17 @@ package com.cydeo.utilities;
 
 public class BrowserUtilities {
 
+/* This method will accept int(in seconds)and execute Thread.sleep
+for given duration
+ */
 
+    public void sleep(int second) {
 
+        second *=1000;
+        try{
+            Thread.sleep(second);
+        }catch(InterruptedException e){
 
+        }
+}
 }
